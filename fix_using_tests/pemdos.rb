d8317d6058410e400303ddfd711337d57969550c
Alfred Wang <alfredwang0707@gmail.com>
2020-09-29 20:05:28 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,10 +1,10 @@
 # don't forget to add: require 'pry'
-require 'pry'
+
 def snake_it_up(string)
   if string[0] == "s"
-   binding.pry
+  
     "ssssssssss" + string
   else
-    binding.pry string
+     string
   end
 end
